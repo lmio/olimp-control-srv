3a9d8dc6148c80589f13bc855024b4aaefe6f9be
Antanas Uršulis <antanas@cantab.net>
2025-03-28 18:56:42 +0000

    Display checkin timestamps in admin

--- ctrl/admin.py
+++ ctrl/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from . import models
 
 
+class CheckInAdmin(admin.ModelAdmin):
+    readonly_fields = ["timestamp"]
+
+
 class UnknownComputerAdmin(admin.ModelAdmin):
     readonly_fields = ["first_seen", "last_seen"]
+
 
 admin.site.register(
     (
         models.Computer,
-        models.CheckIn,
     )
 )
-
+admin.site.register(models.CheckIn, CheckInAdmin)
 admin.site.register(models.UnknownComputer, UnknownComputerAdmin)
