0a115933310198a98abf9df2e60b510eda0626b1
Antanas Uršulis <antanas@cantab.net>
2025-03-28 14:35:08 +0000

    Add basic initial views

--- ctrl/urls.py
+++ ctrl/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="ctrl.index"),
+    path("computer/<str:machine_id>/", views.computer, name="ctrl.computer"),
 ]
--- ctrl/views.py
+++ ctrl/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Computer
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+    computers = Computer.objects.order_by("name")
+    context = {
+        "computers": computers,
+    }
+    return HttpResponse(render(request, "ctrl/index.html", context))
+
+
+def computer(request, machine_id):
+    try:
+        computer = Computer.objects.get(machine_id=machine_id)
+    except Computer.DoesNotExist:
+        raise Http404("Computer does not exist")
+
+    context = {
+        "computer": computer,
+    }
+    return HttpResponse(render(request, "ctrl/computer.html", context))
