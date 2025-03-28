38613cb5b9f01d83b9738134694534ff7bce7bd9
Antanas Uršulis <antanas@cantab.net>
2025-03-28 16:58:29 +0000

    Pings now validate on both ends

--- ctrl/apiviews.py
+++ ctrl/apiviews.py
@@ -1,4 +1,5 @@
 import hmac
+import json
 
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
@@ -9,16 +10,30 @@ from .models import CheckIn, Computer
 
 X_LMIO_AUTH = "X-lmio-auth"
 
+
+def _make_response(status_code, body, hmac_key):
+    resp = HttpResponse(body, status=status_code)
+    resp.headers[X_LMIO_AUTH] = hmac.HMAC(hmac_key, body.encode("utf-8"), "sha1").hexdigest()
+    return resp
+
+
 @csrf_exempt
 def ping(request):
     auth = request.headers.get(X_LMIO_AUTH)
-    body = request.body
 
-    key = "abcde"
-    req_digest = hmac.HMAC(key.encode("utf-8"), body, "sha1").digest()
+    key = "abcde".encode("utf-8")
+    req_digest = hmac.HMAC(key, request.body, "sha1").hexdigest()
 
-    auth_bytes = bytes.fromhex(auth)
+    body = json.loads(request.body)
+    print(body)
 
-    is_good = auth_bytes == req_digest
+    is_good = hmac.compare_digest(auth, req_digest)
 
-    return HttpResponse(f"{is_good}\n{auth_bytes}\n{req_digest}")
+    if is_good:
+        resp_body = json.dumps({"timestamp": body["timestamp"], "status": "200", "message": "OK"})
+        status = 200
+    else:
+        resp_body = "Invalid authentication data"
+        status = 403
+
+    return _make_response(status, resp_body, key)
