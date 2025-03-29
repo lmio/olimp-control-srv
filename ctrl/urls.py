d042c0da392a62f6725e0fc39b686910864ecf74
Antanas Uršulis <antanas@cantab.net>
2025-03-29 17:38:20 +0000

    Some more basic views

--- ctrl/urls.py
+++ ctrl/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="ctrl.index"),
     path("computer/<str:machine_id>/", views.computer, name="ctrl.computer"),
+    path("task_list/", views.TaskListView.as_view(), name="ctrl.task_list"),
+    path("task/<int:pk>/", views.TaskView.as_view(), name="ctrl.task"),
+    path("ticket/<int:pk>/", views.TicketView.as_view(), name="ctrl.ticket"),
 ]
--- ctrl/views.py
+++ ctrl/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.views import generic
 
-from .models import CheckIn, Computer
+from .models import Computer, Task, Ticket
 
 
 @login_required
@@ -18,9 +20,29 @@ def index(request):
 def computer(request, machine_id):
     computer = get_object_or_404(Computer, machine_id=machine_id)
     checkins = computer.checkin_set.order_by("-timestamp")[:10]
+    tickets = computer.ticket_set.order_by("-added")
 
     context = {
         "computer": computer,
         "checkins": checkins,
+        "tickets": tickets,
     }
     return HttpResponse(render(request, "ctrl/computer.html", context))
+
+
+class TaskView(LoginRequiredMixin, generic.DetailView):
+    template_name = "ctrl/task.html"
+    model = Task
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    template_name = "ctrl/task_list.html"
+    context_object_name = "task_list"
+
+    def get_queryset(self):
+        return Task.objects.order_by("-added")
+
+
+class TicketView(LoginRequiredMixin, generic.DetailView):
+    template_name = "ctrl/ticket.html"
+    model = Ticket
